adc0bc1c6475f712a4df0e23f41d8825f265fee0
leonkae <leon29kae@gmail.com>
2022-06-21 12:42:00 +0300

    authentication views, dbs, urls and templates

--- tunzapp/models.py
+++ tunzapp/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+
+class Profile(models.Model):
+    full_name = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(max_length=100)
+    location = models.CharField(max_length=100, blank=True)
+    phone_number = models.CharField(max_length=100, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='profile', null=True)
+
+    def __str__(self):
+        return self.full_name
+    
+    
 class FinancialNeed(models.Model):
     needs = models.CharField(max_length=50, unique=True)
 
@@ -35,12 +49,4 @@ class Child(models.Model):
 
 
 
-class Profile(models.Model):
-    full_name = models.CharField(max_length=200, blank=True)
-    email = models.EmailField(max_length=100)
-    location = models.CharField(max_length=100, blank=True)
-    phone_number = models.CharField(max_length=100, blank=True)
-
-    def __str__(self):
-        return self.full_name
 
--- tunzapp/views.py
+++ tunzapp/views.py
@@ -1,4 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from .models import *
 
 # Create your views here.
 def home(request):
@@ -7,10 +15,44 @@ def home(request):
 
 def signup(request):
     '''view for signup'''
-    return render(request,'tunza/signup.html')
-
-def login(request):
-    '''view for login'''
+    if request.user.is_authenticated:
+        return redirect('login')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                created_user = form.save()
+                Profile.objects.get_or_create(user =created_user)
+                messages.success(request, 'Account for' + username + ' created successfully')
+                return redirect('login')
+            
+    context ={'form': form}        
     
-    return render(request, 'tunza/login.html') 
+    return render(request,'tunza/signup.html', context)
 
+def login_page(request):
+    '''view for login'''
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            print(username,password,user)
+            if user is not None:
+                login(request,user)
+                return redirect ('home')
+            else:
+                messages.info(request, 'Check username or password !')
+        
+        return render(request, 'tunza/login.html')        
+
+
+def logout_user(request):
+    '''logout user'''
+    logout(request)
+    return redirect('login')

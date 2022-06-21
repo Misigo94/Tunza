b88d6bc8b6ca9dd0e0ed5400f400d10bbc7970b1
leonkae <leon29kae@gmail.com>
2022-06-21 11:04:17 +0300

    added auth temlates

--- tunzapp/views.py
+++ tunzapp/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    '''view for home'''
     return render(request, 'tunza/home.html')
+
+def signup(request):
+    '''view for signup'''
+    return render(request,'tunza/signup.html')
+
+def login(request):
+    '''view for login'''
+    
+    return render(request, 'tunza/login.html') 
+

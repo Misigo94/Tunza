f4d18682125b244b7f4d442e15ca0cedb35cdbdb
ludwig <murimiludwig@gmail.com>
2022-06-21 09:56:21 +0300

    creatio of the urls for api calls

--- tunzapp/urls.py
+++ tunzapp/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    
+    path('',views.apiOverview, name='api-overview'),
+    path('api/childList/',views.postList, name="child-list"),
+    path('api/childDetail/<str:pk>/',views.postDetail, name="child-detail"),
+    path('api/childCreate/',views.postCreate, name="child-create"),
+    path('api/childUpdate/<str:pk>/',views.postUpdate, name="child-update"),
+    path('api/childDelete/<str:pk>/',views.postDelete, name="child-delete"),
 ]

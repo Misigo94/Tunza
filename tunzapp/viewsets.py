b10e6cc307d180ad4d4f8c0da9290033d6b638c5
ludwig <murimiludwig@gmail.com>
2022-06-21 09:30:21 +0300

    creation of the import functionalitys for creation of api

--- tunzapp/viewsets.py
+++ tunzapp/viewsets.py
@@ -1,8 +1,15 @@
 from rest_framework import viewsets
 from . import models
 from . import serializers
+from .models import Child
+from .serializers import ChildSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 class ChildViewset(viewsets.ModelViewSet):
     queryset = models.Child.objects.all()
     serializer_class = serializers.ChildSerializer
+ 
+ 
+

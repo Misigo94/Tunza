8b9a9ca384712c456ca17b146bb47b432b4be830
ludwig <murimiludwig@gmail.com>
2022-06-21 09:59:15 +0300

    modifications of the urls.py

--- tunzapp/urls.py
+++ tunzapp/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns=[
     path('',views.apiOverview, name='api-overview'),
-    path('api/childList/',views.postList, name="child-list"),
-    path('api/childDetail/<str:pk>/',views.postDetail, name="child-detail"),
-    path('api/childCreate/',views.postCreate, name="child-create"),
-    path('api/childUpdate/<str:pk>/',views.postUpdate, name="child-update"),
-    path('api/childDelete/<str:pk>/',views.postDelete, name="child-delete"),
+    path('api/childList/',views.childList, name="child-list"),
+    path('api/childDetail/<str:pk>/',views.childDetail, name="child-detail"),
+    path('api/childCreate/',views.childCreate, name="child-create"),
+    path('api/childUpdate/<str:pk>/',views.childUpdate, name="child-update"),
+    path('api/childDelete/<str:pk>/',views.childDelete, name="child-delete"),
 ]
